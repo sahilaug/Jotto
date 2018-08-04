ade4daa066ac0a5a793ef058a18e75e967be133c
Sahil Sharma <sahilaug@gmail.com>
2018-08-04 09:07:04 +0530

    add basic render test

--- src/GuessedWords.test.js
+++ src/GuessedWords.test.js
@@ -15,9 +15,17 @@ const defaultProps = {
  */
 const setup = (props = {}) => {
     const setupProps = { ...defaultProps, ...props};
-    return shallow(<GuessedWords     />)
+    return shallow(<GuessedWords {...setupProps}  />)
 }
 
 test('does not throw warning with expected props', () => {
     checkProps(GuessedWords, defaultProps);
 })
+
+describe('if there are no words guessed', () => {
+    const wrapper = setup({ guesssedWords : []});
+    const component = findByTestAttr(wrapper, 'component-guessed-words')
+    expect(component.length).toBe(1);
+});
+
+describe("if there are words guessed", () => {});
