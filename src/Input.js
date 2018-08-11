65023432dcfd4225ed67a30ceb6b6bae51163a42
Sahil Sharma <sahilaug@gmail.com>
2018-08-11 10:48:08 +0530

    wire input component and tests

--- src/Input.js
+++ src/Input.js
@@ -3,16 +3,26 @@ import { connect } from 'react-redux';
 
 class Input extends Component {
     render() {
-        return (
-            <div>
-                <button ></button>
-            </div>
+        const contents =  this.props.success ? null : (
+            <form className="form-inline">
+                <input
+                    data-test="input-box"
+                    className="mb-2 mx-sm-3"
+                    id="word-guess"
+                    type="text"
+                    placeholder="Enter Guess"
+                />
+                <button data-test="submit-button" type="submit" className="btn btn-primary mb2">Submit</button>
+            </form>
         );
+        return <div data-test="component-input">{contents}</div>;
     }
 }
 
-const mapStateToProps = (state) => {
-    return {};
+const mapStateToProps = ({ success }) => {
+    return {
+        success
+    };
 }
 
 export default connect(mapStateToProps)(Input);
